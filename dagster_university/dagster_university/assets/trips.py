746b3672297de637a19b633bbb25eeb6fdec57f8
colton <colton.padden@fastmail.com>
2023-11-15 15:46:27 -0500

    Complete - Lesson 3: Software-defined Assets

--- dagster_university/dagster_university/assets/trips.py
+++ dagster_university/dagster_university/assets/trips.py
@@ -1,2 +1,26 @@
 import requests
+from dagster import asset
+
 from . import constants
+
+
+@asset
+def taxi_trips_file():
+    """Raw parquet files of taxi trips; sourced from the NYC Open Data portal.
+    """
+    month_to_fetch = '2023-03'
+    raw_trips = requests.get(
+        f"https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_{month_to_fetch}.parquet"
+    )
+    with open(constants.TAXI_TRIPS_TEMPLATE_FILE_PATH.format(month_to_fetch), "wb") as output_file:
+        output_file.write(raw_trips.content)
+
+@asset
+def taxi_zones_file():
+    """Raw CSV file of taxi zones; sourced from the NYC Open Data portal.
+    """
+    response = requests.get(
+        f"https://data.cityofnewyork.us/api/views/755u-8jsi/rows.csv?accessType=DOWNLOAD"
+    )
+    with open(constants.TAXI_ZONES_FILE_PATH, "wb") as output_file:
+        output_file.write(response.content)
